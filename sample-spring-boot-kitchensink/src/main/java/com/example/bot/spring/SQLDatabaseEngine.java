382af775e85bb45c008c33e011ebde75b3325450
CHONG Wai Yeung <wychong@WIN2K.CS.UST.HK>
2017-09-28 18:46:43 +0800

    aaa

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,6 +5,9 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
@@ -12,8 +15,34 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
+		
+
+		try {
+
+			Connection connection=getConnection();
+		
+		
+		PreparedStatement stmt = connection.prepareStatement(
+				"SELECT keyword, response FROM chatbot;");
+		
+
+		ResultSet rs = stmt.executeQuery();
+		
+		while (rs.next()) {
+			 if (text.toLowerCase().equals(rs.getString(1).toLowerCase())) 
+			return rs.getString(2);}
+		
+		
+		rs.close();
+		stmt.close();
+		connection.close();
+		
+		} catch (Exception e){
+		System.out.println(e);
+		}
 		return null;
 	}
+
 	
 	
 	private Connection getConnection() throws URISyntaxException, SQLException {
@@ -32,4 +61,4 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 		return connection;
 	}
 
-}
+	}
